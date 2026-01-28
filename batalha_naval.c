ee5ba19ce58983bdf4d9c104230a59b8351dc565
Valdir Lima <valdir.engsoftware@gmail.com>
2026-01-28 13:25:28 -0300

    feat: posiciona navios horizontal e vertical com validacao

--- batalha_naval.c
+++ batalha_naval.c
@@ -2,19 +2,41 @@
 
 #define TAM 10
 #define AGUA 0
+#define NAVIO 3
+#define NAVIO_TAM 3
 
 int main() {
     int tabuleiro[TAM][TAM];
 
-    // Inicializa o tabuleiro com água (0)
+    // 1. Inicializa o tabuleiro com água (0)
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
             tabuleiro[i][j] = AGUA;
         }
     }
 
-    // Exibe o tabuleiro no console
-    printf("Tabuleiro de Batalha Naval (10x10):\n\n");
+    // 2. Posiciona navio horizontal
+    int linha_h = 2;
+    int coluna_h = 3;
+
+    if (coluna_h + NAVIO_TAM <= TAM) {
+        for (int i = 0; i < NAVIO_TAM; i++) {
+            tabuleiro[linha_h][coluna_h + i] = NAVIO;
+        }
+    }
+
+    // 3. Posiciona navio vertical
+    int linha_v = 5;
+    int coluna_v = 6;
+
+    if (linha_v + NAVIO_TAM <= TAM) {
+        for (int i = 0; i < NAVIO_TAM; i++) {
+            tabuleiro[linha_v + i][coluna_v] = NAVIO;
+        }
+    }
+
+    // 4. Exibe o tabuleiro
+    printf("Tabuleiro de Batalha Naval (Navios Horizontal e Vertical):\n\n");
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
             printf("%d ", tabuleiro[i][j]);
